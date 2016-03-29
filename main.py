f0488f0acefdcbb139d82f1e728fc98ee1edb7c2
Luis Valenzuela <lav62@nau.edu>
2016-03-29 16:15:38 -0700

    Broken send function

---
+++ main.py
@@ -0,0 +1,23 @@
+__author__ = 'Luis Valenzuela'
+
+import smtplib
+import time
+from time import gmtime, strftime, localtime
+
+username = "pythonprogram123@gmail.com"
+password = "python123"
+fromaddr = "pythonprogram123@gmail.com"
+toaddrs = "lav62@nau.edu"
+msg = "Hello!"
+
+
+def doneTextSend() :
+
+    # The actual mail send
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
+
+doneTextSend()
